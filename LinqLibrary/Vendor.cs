9b0a5c931b6c23baa854c206d9b6b32be3fb7524
Greg Doud <gpdoud@gmail.com>
2019-02-08 08:14:39 -0500

    Initial commit

---
+++ LinqLibrary/Vendor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLibrary {
+
+    public class Vendor {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Discount { get; set; }
+
+        public Vendor() { }
+    }
+}
---
+++ TestLinqLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqLibrary;
+
+namespace TestLinqLibrary {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine(LinqLibrary.Linq.About);
+            var msprods = from p in Linq.Products
+                          join v in Linq.Vendors 
+                          on p.VendorId equals v.Id
+                          where v.Name.Equals("Amazon")
+                          orderby p.Price descending
+                          //select new { p.Name };
+                          select p;
+            foreach(var p in msprods) {
+                p.Print();
+            }
+            Console.ReadKey();
+        }
+    }
+}
